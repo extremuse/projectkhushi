388ed783cee45e668f1b7a68a4ee738fcc0dcdbb
Sachin kumar <113299714+extremuse@users.noreply.github.com>
2023-08-22 23:31:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
     document.querySelector("html").style.scrollBehavior = "unset";
   }, 1000);
 });
+/// form//
+
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_0xs9n6m";
+    const templateID = "template_vhei6ni";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
